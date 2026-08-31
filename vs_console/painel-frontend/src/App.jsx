43281200f642e49d0bcf56d3aa437a5e62385339
illee.silva <illee.silvalb@gmail.com>
2026-08-31 18:17:37 -0300

    Versão 1 do Frontend

--- vs_console/painel-frontend/src/App.jsx
+++ vs_console/painel-frontend/src/App.jsx
@@ -1,121 +1,213 @@
-import { useState } from 'react'
+import { useEffect, useState, useRef} from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import { FaPlay, FaStop, FaRedo } from 'react-icons/fa';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [status, setStatus] = useState('')
+  const [command, setCommand] = useState('')
+
+
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    const listenkeyboard = (e) => {
+      if (e.key === '/' && document.activeElement !== inputRef.current){
+
+        e.preventDefault();
+        inputRef.current.focus()
+        setCommand('/')
+
+      }
+    };
+
+    window.addEventListener('keydown', listenkeyboard);
+    
+    return () => {
+      window.removeEventListener('keydown', listenkeyboard)
+    };
+
+  }, [])
+
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className='main-container'>
 
-      <div className="ticks"></div>
+      <div className='buttons-bar'>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        <h2 className='btbartittle'>Server Dashboard</h2>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+        <div className='btgroup'>
+          <button className='bt start'>
+            <FaPlay />
+          </button>
+
+          <button className='bt stop'>
+            <FaStop />
+          </button>
+
+          <button className='bt redo'>
+            <FaRedo />
+          </button>
+
+          </div>
+        
+      </div>
+      
+      <div className='content-area'>
+
+        <div className='console-window'>
+          <div className='console-window-tittle'>
+            Terminal Output
+          </div>
+          <div className='console-window-content'>
+
+          <p>[14:00:01] Servidor Vintage Story iniciado...</p>
+          <p>[14:00:01] O Player thyfnn se conectou!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          <p>[14:00:01] O servidor está sobrecarregado!</p>
+          
+          
+          </div>
+
+          <div className='console-input-bar'>
+            <span style={{color: '#4caf50', fontWeight: "bold"}}>&gt;</span>
+            <input
+              type='text'
+              ref={inputRef}
+              className='console-input'
+              placeholder='Type any command...'
+              value = {command}
+              onChange={(e) => setCommand(e.target.value)}
+               onKeyDown={(e) => e.key === 'Enter' && setCommand('')}
+            />
+          </div>
+
+        </div>
+
+        <div className='info-window'>
+          <div className='info-window tittle'>
+            Status
+          </div>
+          <div className='info-window content'>
+            <p><strong>Estado:</strong> Online</p>
+            <p><strong>CPU:</strong> 15%</p>
+            <p><strong>RAM:</strong> 2.4 GB</p>
+            <p><strong>Jogadores:</strong> 3/16</p>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
